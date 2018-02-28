fe5e532770323ca337a7237b37848cdc524dd430
hudi <hellohudi@163.com>
2018-02-28 11:56:06 +0800

    update

--- app/src/main/java/com/hd/tools/view/ChoosePatternActivity.kt
+++ app/src/main/java/com/hd/tools/view/ChoosePatternActivity.kt
@@ -4,8 +4,6 @@ import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
 import com.hd.tools.R
-import com.hd.tools.help.ShowVideo
-import kotlinx.android.synthetic.main.activity_choose_pattern.*
 import org.jetbrains.anko.startActivity
 
 /**
@@ -14,20 +12,12 @@ import org.jetbrains.anko.startActivity
  */
 class ChoosePatternActivity : BaseActivity() {
 
-    private val showVideo by lazy { ShowVideo(this, bgVideoView, R.raw.health_live, "health_live.mp4") }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         }
         setContentView(R.layout.activity_choose_pattern)
-//        showVideo.showVideo()
-    }
-
-    override fun onStop() {
-        super.onStop()
-//        showVideo.stopVideo()
     }
 
     /** serial port device measure*/
